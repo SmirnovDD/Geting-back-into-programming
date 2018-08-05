db111bc2bde5554295600715e12f530f16eb36c3
денис денис <123432145@bk.ru>
2018-08-05 22:38:15 +0300

    Initial commit

---
+++ Assets/Scripts/MyCameraController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCameraController : MonoBehaviour {
+    public Vector2 mouseSensitivity; 
+    private Transform camTransform, charTransform;
+
+	// Use this for initialization
+	void Start ()
+    {
+        charTransform = transform;
+        camTransform = Camera.main.transform;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(Input.GetAxisRaw("Mouse X") != 0)
+        {
+            charTransform.rotation *= Quaternion.AngleAxis(Input.GetAxisRaw("Mouse X") * mouseSensitivity.x, Vector3.up);
+        }
+
+        if(Input.GetAxisRaw("Mouse Y") != 0)
+        {
+            camTransform.rotation *= Quaternion.AngleAxis(Input.GetAxisRaw("Mouse Y") * -mouseSensitivity.y, Vector3.right);
+        }
+
+    }
+}
---
+++ Assets/Scripts/MyCharacterController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCharacterController : MonoBehaviour {
+    public CharacterController charController;
+    public float movementSpeed;
+    private Transform playerTransform;
+
+    void Start()
+    {
+        playerTransform = transform;
+    }
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if(Input.GetAxis("Vertical") != 0)
+        {
+            charController.transform.position += playerTransform.forward * Input.GetAxisRaw("Vertical") * Time.deltaTime * movementSpeed;
+        }
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            charController.transform.position += playerTransform.right * Input.GetAxisRaw("Horizontal") * Time.deltaTime * movementSpeed;
+        }
+    }
+    
+}
+
